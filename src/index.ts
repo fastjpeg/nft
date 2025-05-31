c91e489d0b1ccc4214b790e4daf46dd310ddb5d1
Joe Blau <im@joeblau.com>
2025-05-31 10:23:42 -0700

    feat: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import type { NFT, NFTData } from './lib/types';
-import { URLScheme, getScheme } from './lib/scheme';
-import { nft } from './client/nft';
-import { getCID } from './lib/ipfs';
-import { getTransactionId } from './lib/arweave';
-
-export type { NFT, NFTData };
-
-export { URLScheme, nft, getCID, getTransactionId, getScheme };
+export * from './lib/types';
+export * from './lib/scheme';
+export * from './client/nft';
+export * from './lib/ipfs';
+export * from './lib/arweave';
